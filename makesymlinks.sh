665aaf680da37db5b24b6490705ff02531e82d01
michael.mead <michael.mead@vanderbilt.edu>
2019-04-26 12:38:09 -0500

    add cheat sheet script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -16,7 +16,7 @@ alacrittydir=~/.config/alacritty            # nvim directory
 homeFiles="bashrc vim viminfo tmux.conf ctags gitignore_global Xresources"    # list of files/folders to symlink in homedir
 configFiles="pycodestyle flake8 .pylintrc"
 nvimFiles="vimrc"
-binFiles="rdrdev.sh, gitlog.sh"
+binFiles="rdrdev.sh, gitlog.sh, cht.sh"
 
 ##########
 

a82f854b1eb38bf14a3aecd4f920af9b2a7bccf9
michael.mead <michaelmead007@icloud.com>
2022-10-27 21:31:13 -0500

    update settings for mason and lsp install

--- lua/lspv2.lua
+++ lua/lspv2.lua
@@ -165,10 +165,10 @@ cmp.setup({
 -- Language servers
 ---
 require("mason").setup()
-local to_install = { "sumneko_lua", "rust_analyzer", "gopls", "pylsp", "flake8", "pylint" }
+local to_install = { "sumneko_lua", "rust_analyzer", "gopls", "pylsp" }
 require("mason-lspconfig").setup({
     ensure_installed = to_install,
-    automatic_installation = true
+    automatic_installation = false
 })
 
 -- local servers = { 'tsserver', 'gopls', 'pylsp'}

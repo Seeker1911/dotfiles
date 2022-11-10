53b2db91a7e3e5af2523b10a6943d61edce9c25e
michael.mead <michaelmead007@icloud.com>
2022-11-10 12:50:32 -0600

    update lualine with diagnositcs and lsp info

--- lua/lualinesetup.lua
+++ lua/lualinesetup.lua
@@ -1,61 +1,75 @@
-local lualine_diag = {
-    {
-      'diagnostics',
-
-      -- Table of diagnostic sources, available sources are:
-      --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
-      -- or a function that returns a table as such:
-      --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
-      sources = { 'nvim_diagnostic', 'nvim_lsp' },
-
-      -- Displays diagnostics for the defined severity types
-      sections = { 'error', 'warn', 'info', 'hint' },
-
-      diagnostics_color = {
-        -- Same values as the general color option can be used here.
-        error = 'DiagnosticError', -- Changes diagnostics' error color.
-        warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
-        info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
-        hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
-      },
-      symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
-      colored = true,           -- Displays diagnostics status in color if set to true.
-      update_in_insert = true, -- Update diagnostics in insert mode.
-      always_visible = true,   -- Show diagnostics even if there are none.
-    }
-  }
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'gruvbox_dark',
-    -- theme = 'gruvbox',
-    -- theme = 'gruvbox-material',
-    -- theme = 'everforest',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
-    always_divide_middle = true,
-    globalstatus = false,
-  },
-  sections = {
-    lualine_a = {'mode'},
-    -- lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_b = { {'FugitiveHead', icon = ''}, },
-    -- lualine_c = {'filename',"require'lsp-status'.status()", 'FugitiveStatusline()' },
-    lualine_c = lualine_diag,
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {'fugitive', 'fzf'}
+local lualine = require('lualine')
+local space = {
+    function()
+        return '%='
+    end,
+}
+local lsp_server = {
+    -- Lsp server name .
+    function()
+        local msg = 'No Active Lsp'
+        local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+        local clients = vim.lsp.get_active_clients()
+        if next(clients) == nil then
+            return msg
+        end
+        for _, client in ipairs(clients) do
+            local filetypes = client.config.filetypes
+            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                return client.name
+            end
+        end
+        return msg
+    end,
+    icon = '  LSP:',
+    -- color = { fg = '#ffffff', gui = 'bold' },
+}
+local lualine_diag =
+{
+    'diagnostics',
+    sources = { 'nvim_diagnostic' },
+    symbols = { error = ' ', warn = ' ', info = ' ' },
+    diagnostics_color = {
+        color_error = { fg = 'DiagnosticError' },
+        color_warn = { fg = 'DiagnosticWarn' },
+        color_info = { fg = 'diagnosticInfo' },
+    },
+    colored = true, -- Displays diagnostics status in color if set to true.
+    update_in_insert = true, -- Update diagnostics in insert mode.
+    always_visible = true, -- Show diagnostics even if there are none.
 }
 
+lualine.setup {
+    options = {
+        icons_enabled = true,
+        theme = 'gruvbox_dark',
+        -- theme = 'gruvbox',
+        -- theme = 'gruvbox-material',
+        -- theme = 'everforest',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {},
+        always_divide_middle = true,
+        globalstatus = false,
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        -- lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_b = { { 'FugitiveHead', icon = '' }, },
+        -- lualine_c = {'filename',"require'lsp-status'.status()", 'FugitiveStatusline()' },
+        lualine_c = { lualine_diag, space,  lsp_server },
+        lualine_x = { 'diff', 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
+    extensions = { 'fugitive', 'fzf' }
+}

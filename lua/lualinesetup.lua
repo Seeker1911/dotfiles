061d0f6204bd996c1b52b20f841ddb5a33f10c2d
michael.mead <michaelmead007@icloud.com>
2022-11-10 13:05:44 -0600

    cleanup lualine

--- lua/lualinesetup.lua
+++ lua/lualinesetup.lua
@@ -1,9 +1,31 @@
 local lualine = require('lualine')
+
+local conditions = {
+    buffer_not_empty = function()
+        return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+    end,
+    hide_in_width = function()
+        return vim.fn.winwidth(0) > 80
+    end,
+    check_git_workspace = function()
+        local filepath = vim.fn.expand('%:p:h')
+        local gitdir = vim.fn.finddir('.git', filepath .. ';')
+        return gitdir and #gitdir > 0 and #gitdir < #filepath
+    end,
+}
+
+local filename = {
+    'filename',
+    cond = conditions.buffer_not_empty,
+}
+
 local space = {
     function()
         return '%='
     end,
+    cond = conditions.hide_in_width,
 }
+
 local lsp_server = {
     -- Lsp server name .
     function()
@@ -24,8 +46,8 @@ local lsp_server = {
     icon = '  LSP:',
     -- color = { fg = '#ffffff', gui = 'bold' },
 }
-local lualine_diag =
-{
+
+local lualine_diag = {
     'diagnostics',
     sources = { 'nvim_diagnostic' },
     symbols = { error = ' ', warn = ' ', info = ' ' },
@@ -37,6 +59,7 @@ local lualine_diag =
     colored = true, -- Displays diagnostics status in color if set to true.
     update_in_insert = true, -- Update diagnostics in insert mode.
     always_visible = true, -- Show diagnostics even if there are none.
+    cond = conditions.hide_in_width,
 }
 
 lualine.setup {
@@ -54,10 +77,8 @@ lualine.setup {
     },
     sections = {
         lualine_a = { 'mode' },
-        -- lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_b = { { 'FugitiveHead', icon = '' }, },
-        -- lualine_c = {'filename',"require'lsp-status'.status()", 'FugitiveStatusline()' },
-        lualine_c = { lualine_diag, space,  lsp_server },
+        lualine_b = { { 'FugitiveHead', icon = '' }, filename },
+        lualine_c = { lualine_diag, space, lsp_server },
         lualine_x = { 'diff', 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }

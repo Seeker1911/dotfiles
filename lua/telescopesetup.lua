b036b5256f8495cd7e2e81f74c793369c1fe6622
michael.mead <michaelmead007@icloud.com>
2022-11-21 10:22:09 -0600

    add closing brace

--- lua/telescopesetup.lua
+++ lua/telescopesetup.lua
@@ -54,6 +54,7 @@ require('telescope').setup{
       },
   }
 }
+}
 -- you need to call load_extension, somewhere after setup function:
 require("telescope").load_extension "file_browser"
 require("telescope").load_extension "gh"

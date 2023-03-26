1363d05fd6a7b68a5688ebed225ccac38f6e2d5c
michael.mead <michaelmead007@icloud.com>
2023-03-26 10:26:18 -0500

    add cmp bahavior

--- lua/cmpsetup.lua
+++ lua/cmpsetup.lua
@@ -70,7 +70,10 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
 
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ 
+                select = true,
+                behavior = cmp.ConfirmBehavior.Replace,
+    }),
 
 
         ['<Tab>'] = cmp.mapping(function(fallback)

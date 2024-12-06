44516a73bc64a162852ec60273e3c52be9478975
michael.mead <michaelmead007@icloud.com>
2024-12-06 14:53:18 -0600

    add lazy git

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,33 +1,26 @@
 -- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
 -- Please read that file to know all available options :(
-
 ---@type ChadrcConfig
-local M = {
-  base46 = {
-    theme = "flex-light",
-    -- hl_override = {
-    -- 	Comment = { italic = true },
-    -- 	["@comment"] = { italic = true },
-    -- },
-    ui = {
-      cmp = {
-        lspkind_text = true,
-        style = "default", -- default/flat_light/flat_dark/atom/atom_colored
-        format_colors = {
-          tailwind = false,
-        },
-      },
-      statusline = {
-        -- |values| = default, vscode, vscode_colored, minimal
-        theme = "minimal", -- default/vscode/vscode_colored/minimal
-        -- default/round/block/arrow separators work only for default statusline theme
-        -- round and block will work for minimal theme only
-        separator_style = "default",
-        order = nil,
-        modules = nil,
-      },
-    },
-  },
+local M = {}
+
+M.base46 = {
+	theme = "github_light",
+	-- Other configurations...
 }
+
+M.ui = {
+	cmp = {
+		lspkind_text = true,
+		style = "default",
+		format_colors = {
+			tailwind = false,
+		},
+	},
+	statusline = {
+		theme = "vscode_colored",
+		separator_style = "default",
+	},
+}
+
 return M

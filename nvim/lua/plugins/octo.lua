04b491c3d5b4efc91b88f1e732e1acacd0fa3ddd
michael.mead <michaelmead007@icloud.com>
2026-01-27 17:12:34 -0600

    update octo config to include v2

--- nvim/lua/plugins/octo.lua
+++ nvim/lua/plugins/octo.lua
@@ -1,16 +1,18 @@
 return {
-	"pwntester/octo.nvim",
-	-- event = "UIEnter",
-	cmd = "Octo",
-	enabled = true,
-	opts = {},
-	requires = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope.nvim",
-		-- OR 'ibhagwan/fzf-lua',
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require("octo").setup()
-	end,
+    "pwntester/octo.nvim",
+    -- event = "UIEnter",
+    cmd = "Octo",
+    enabled = true,
+    opts = {
+        default_to_projects_v2 = true
+    },
+    requires = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+        -- OR 'ibhagwan/fzf-lua',
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("octo").setup()
+    end,
 }

a503f3629ffd67c01c49bc2cb064001592643c57
michael.mead <michaelmead007@icloud.com>
2026-02-05 13:05:40 -0600

    show kind icons for autocomplete

--- nvim/lua/configs/blink.lua
+++ nvim/lua/configs/blink.lua
@@ -31,6 +31,13 @@ return {
         },
         menu = {
             border = "rounded",
+            draw = {
+                columns = {
+                    { "kind_icon" },
+                    { "label",      "label_description", gap = 1 },
+                    { "source_name" },
+                },
+            },
         },
     },
 
